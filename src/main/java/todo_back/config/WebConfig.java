be24e40802afe0f0934686553b07b35668b4c61a
nao317 <country.gentleman.0317@gmail.com>
2025-07-14 12:06:04 +0900

    ドメイン修正

--- src/main/java/todo_back/config/WebConfig.java
+++ src/main/java/todo_back/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
         .allowedOrigins("http://localhost:3000",
-        "https://my-todo-frontend-three.vercel.app/" /* 本番環境用 */
+        "https://my-todo-frontend-three.vercel.app" /* 本番環境用 */
         )
         .allowedMethods("GET", "POST", "PUT", "DELETE")
         .allowCredentials(true);
